a18db822b9697686dac4646f3d86443b207e853b
Leon <leon.karho.chung@gmail.com>
2018-07-18 16:41:00 -0400

    express middleware, import routes

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ const express = require('express')
 const mongoose = require('mongoose')
 const { mongoURI } = require('./config/keys')
 
+// load routes
+const users = require('./routes/api/users')
+
 const app = express()
+
+// middleware
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+// router
+app.use('/api/users', users)
 
 // mongoose connection
 mongoose
-  .connect(mongoURI)
+  .connect(mongoURI, {useNewUrlParser:true})
   .then(console.log('*****Mongoose connected*****'))
+  .catch(err => console.log(err))
 
+// local server
 const port = process.env.PORT || 8080
-
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`)
 })
