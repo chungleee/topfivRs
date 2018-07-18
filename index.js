339dea7ec710bf6f9fa7d797c72c508592f01d92
Leon <leon.karho.chung@gmail.com>
2018-07-18 16:18:12 -0400

    mongoose connection setup

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const { mongoURI } = require('./config/keys')
 
 const app = express()
+
+
+// mongoose connection
+mongoose
+  .connect(mongoURI)
+  .then(console.log('*****Mongoose connected*****'))
 
 const port = process.env.PORT || 8080
 
