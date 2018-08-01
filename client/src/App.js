ee3985080233ea734d12c04033cf01613a4844f7
Leon <leon.karho.chung@gmail.com>
2018-08-01 15:39:40 -0400

    react routing setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
+import 'bulma/css/bulma.css'
+import Navbar from './components/common/Navbar';
+import Landing from './components/landing/Landing';
+import Register from './components/register/Register'
+import Login from './components/login/Login'
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello world</h1>
+      <Router>
+        <div>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <Route path='/register' component={Register} />
+          <Route path='/login' component={Login} />
+        </div>
+      </Router>
     );
   }
 }
