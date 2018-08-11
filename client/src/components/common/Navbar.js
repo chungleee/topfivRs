f17a4b7d4961f1754552bbf4633795f60de6349e
Leon <leon.karho.chung@gmail.com>
2018-08-11 15:30:10 -0400

    renderForms func based on props.auth

--- client/src/components/common/Navbar.js
+++ client/src/components/common/Navbar.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
+  renderForms = () => {
+    if(!this.props.auth) {
+      return (
+        <div className="navbar-menu is-active">               
+          <div className="navbar-end">
+            <Link to='/search' className='navbar-item'>Search</Link>
+            <Link to='/register' className="navbar-item">Register</Link>
+            <Link to='/login' className="navbar-item">Login</Link>
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div className="navbar-menu is-active">
+            <div className="navbar-end">
+              <Link to='/search' className='navbar-item'>Search</Link>
+              <Link to='/' className="navbar-item">Sign Out</Link>
+            </div>
+          </div>
+      )
+    }
+  }
   render() {
     return (
       <nav className="navbar is-white">
@@ -9,13 +31,7 @@ class Navbar extends Component {
           <div className="navbar-brand">
             <Link to='/' className="navbar-item">TopFivRs</Link>
           </div>
-          <div className="navbar-menu is-active">
-            <div className="navbar-end">
-              <Link to='/register' className="navbar-item">Register</Link>
-              <Link to='/login' className="navbar-item">Login</Link>
-              <Link to='/search' className='navbar-item'>Search</Link>
-            </div>
-          </div>
+          {this.renderForms()}
         </div>
       </nav>
     )
