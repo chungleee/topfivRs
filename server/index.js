7fdf4a131ddb14147d2f08105d1927a5e02687f9
Leon <leon.karho.chung@gmail.com>
2018-08-13 18:24:59 -0400

    please deploy pls

--- server/index.js
+++ server/index.js
@@ -21,10 +21,14 @@ app.use('/api/yelp', yelp)
 
 // serve static assets if in prod
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'))
+  // app.use(express.static('../client/build'))
+  app.use(express.static(path.join(__dirname, '../client/build')))
 
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+  // })
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname + '../client/build/index.html'))
   })
 }
 
